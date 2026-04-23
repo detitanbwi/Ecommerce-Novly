85fd74ea3dc0993372559a591c7009539f89a39a
Titan Anggaraksa <detitanbwi@gmail.com>
2026-04-23 11:43:54 +0700

    Cleanup sitemap formatting

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -1,17 +1,17 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    <url>
-        <loc>{{ url('/') }}</loc>
-        <lastmod>{{ now()->tz('UTC')->toAtomString() }}</lastmod>
-        <changefreq>daily</changefreq>
-        <priority>1.0</priority>
-    </url>
-    @foreach ($products as $product)
-        <url>
-            <loc>{{ route('product.show', $product->slug ?: $product->id) }}</loc>
-            <lastmod>{{ $product->updated_at->tz('UTC')->toAtomString() }}</lastmod>
-            <changefreq>weekly</changefreq>
-            <priority>0.8</priority>
-        </url>
-    @endforeach
+<url>
+<loc>{{ url('/') }}</loc>
+<lastmod>{{ now()->tz('UTC')->toAtomString() }}</lastmod>
+<changefreq>daily</changefreq>
+<priority>1.0</priority>
+</url>
+@foreach ($products as $product)
+<url>
+<loc>{{ route('product.show', $product->slug ?: $product->id) }}</loc>
+<lastmod>{{ $product->updated_at->tz('UTC')->toAtomString() }}</lastmod>
+<changefreq>weekly</changefreq>
+<priority>0.8</priority>
+</url>
+@endforeach
 </urlset>
